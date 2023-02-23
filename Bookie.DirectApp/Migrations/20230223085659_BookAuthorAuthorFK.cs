b4ff2e57d9ec689c79e0f6ca83ad9a324706560c
Alexandra Savescu <alexandrasavescu92@gmail.com>
2023-02-23 11:05:05 +0200

    Add FK - BookAuthor.AuthorId to Author.AuthorId

---
+++ Bookie.DirectApp/Migrations/20230223085659_BookAuthorAuthorFK.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookie.DirectApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookAuthorAuthorFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_BookAuthor_AuthorId",
+                table: "BookAuthor",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookAuthor_Author_AuthorId",
+                table: "BookAuthor",
+                column: "AuthorId",
+                principalTable: "Author",
+                principalColumn: "AuthorId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookAuthor_Author_AuthorId",
+                table: "BookAuthor");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BookAuthor_AuthorId",
+                table: "BookAuthor");
+        }
+    }
+}

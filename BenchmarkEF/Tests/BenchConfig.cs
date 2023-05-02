33b89c941760422e21ba74cd54dd962509654543
Alexandra Savescu <alexandrasavescu92@gmail.com>
2023-05-02 12:13:55 +0300

    benchmark configurable

---
+++ BenchmarkEF/Tests/BenchConfig.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Exporters;
+
+namespace BenchmarkEF.Tests
+{
+    
+    public class BenchConfig : ManualConfig
+    {
+        public BenchConfig()
+        {
+            AddExporter(CsvMeasurementsExporter.Default);
+            AddExporter(RPlotExporter.Default);
+        }
+    }
+}
